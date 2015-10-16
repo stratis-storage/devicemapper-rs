1631d7ffae30a902b095453b1d8c7f240025fc1c
Andy Grover <agrover@redhat.com>
2015-10-15 21:18:16 -0700

    Flesh out dmtest some more
    It looks like our output parsing is working ok, since list_devices and
    list_versions both work, but how we're formatting the input buffer is
    still wrong because we're failing to set name correctly for table_status,
    see #4.
    
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- src/bin/dmtest.rs
+++ src/bin/dmtest.rs
@@ -3,16 +3,21 @@ extern crate devicemapper;
 use devicemapper as dm;
 
 fn main() {
-    println!("Hello, world!");
-
     let dmi = dm::DM::new().unwrap();
 
+    println!("Calling version()");
     let x = dmi.version().unwrap();
     println!("{:?}", x);
 
+    println!("Calling list_devices()");
     let x = dmi.list_devices(dm::DmFlags::empty()).unwrap();
     println!("{:?}", x);
 
+    println!("Calling list_versions()");
+    let x = dmi.list_versions(dm::DmFlags::empty()).unwrap();
+    println!("{:?}", x);
+
+    println!("Calling table_status()");
     let x = dmi.table_status("zon-swap", dm::DmFlags::empty()).unwrap();
     println!("{:?}", x);
 
