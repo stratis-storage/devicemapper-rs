0a715ed219deb43fb25f36c368cb9909410c9daf
Todd Gill <tgill@redhat.com>
2017-03-29 14:46:53 -0400

    Move from Stratis - constants for International Electrotechnical Commission Units Standards.
    Signed-off-by: Todd Gill <tgill@redhat.com>

--- src/consts.rs
+++ src/consts.rs
@@ -4,3 +4,22 @@
 
 /// disk sector size in bytes
 pub const SECTOR_SIZE: usize = 512;
+
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+/// International Electrotechnical Commission Units Standards
+pub mod IEC {
+    /// kibi
+    pub const Ki: u64 = 1024;
+    /// mebi
+    pub const Mi: u64 = 1024 * Ki;
+    /// gibi
+    pub const Gi: u64 = 1024 * Mi;
+    /// tebi
+    pub const Ti: u64 = 1024 * Gi;
+    /// pebi
+    pub const Pi: u64 = 1024 * Ti;
+    /// exbi
+    pub const Ei: u64 = 1024 * Pi;
+    // Ei is the maximum IEC unit expressible in u64.
+}
