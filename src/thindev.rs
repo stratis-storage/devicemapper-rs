450b2ef8de66889824b88dd08439a864f20fc05a
mulhern <amulhern@redhat.com>
2017-04-13 16:51:53 -0400

    Remove unused imports
    Because they are unused.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/thindev.rs
+++ src/thindev.rs
@@ -3,9 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use std::fmt;
-use std::path::Path;
 use std::path::PathBuf;
-use std::process::Command;
 
 use {DM, DevId, DeviceInfo, DmFlags};
 use result::{DmResult, DmError, InternalError};
