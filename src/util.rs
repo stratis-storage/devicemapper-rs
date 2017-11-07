dc69a8e56fe48595f14bb702ccca87ed6a50c91b
mulhern <amulhern@redhat.com>
2017-11-07 08:52:50 -0500

    Omit some unnecessary allows
    For general tidiness.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/util.rs
+++ src/util.rs
@@ -2,8 +2,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#![allow(missing_docs)]
-
 /// The smallest number divisible by `align_to` and at least `num`.
 /// Precondition: `align_to` is a power of 2.
 /// Precondition: `num` + `align_to` < usize::MAX + 1.
