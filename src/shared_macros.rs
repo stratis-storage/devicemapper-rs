011556a2f52ce0d8b425d6faf7e0a2c2e825c742
mulhern <amulhern@redhat.com>
2018-01-30 14:28:53 -0500

    Alter some comments to avoid clippy error
    The precise error is empty_line_after_outer_attr
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/shared_macros.rs
+++ src/shared_macros.rs
@@ -2,8 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-/// A module to contain functionality shared among the various types of
-/// devices and implemented by means of macros.
+// A module to contain functionality shared among the various types of
+// devices and implemented by means of macros.
 
 macro_rules! device {
     ($s: ident) => {
