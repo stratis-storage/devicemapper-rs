d59a218d6f422ff6ff61ecee9cd2dbc90e615d6d
Grace Chin <gracechin15@gmail.com>
2019-04-09 15:54:30 -0400

    -Wrote macros to implement the traits Add, Sub, AddAssign, SubAssign, Deref, From -Replaced calls to Newtype macros with these new macros -Removed the macro_attr and newtype_derive dependencies from definition of units -Build size reduced by 2.36% (was 9241466, is now 9023322)
    rebase

--- src/lib.rs
+++ src/lib.rs
@@ -66,11 +66,6 @@
 #![warn(missing_docs)]
 
 #[macro_use]
-extern crate macro_attr;
-#[macro_use]
-extern crate newtype_derive;
-
-#[macro_use]
 extern crate bitflags;
 #[macro_use]
 extern crate error_chain;
