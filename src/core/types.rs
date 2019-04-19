bffd8cd061a34fc154878228f91e8f95dc80700c
Grace Chin <gracechin15@gmail.com>
2019-04-19 14:59:47 -0400

    turned unused doc comments into regular comments

--- src/core/types.rs
+++ src/core/types.rs
@@ -16,13 +16,13 @@ fn err_func(err_msg: &str) -> DmError {
     DmError::Core(ErrorKind::InvalidArgument(err_msg.into()).into())
 }
 
-/// A devicemapper name. Really just a string, but also the argument type of
-/// DevId::Name. Used in function arguments to indicate that the function
-/// takes only a name, not a devicemapper uuid.
+// A devicemapper name. Really just a string, but also the argument type of
+// DevId::Name. Used in function arguments to indicate that the function
+// takes only a name, not a devicemapper uuid.
 str_id!(DmName, DmNameBuf, DM_NAME_LEN, err_func);
 
-/// A devicemapper uuid. A devicemapper uuid has a devicemapper-specific
-/// format.
+// A devicemapper uuid. A devicemapper uuid has a devicemapper-specific
+// format.
 str_id!(DmUuid, DmUuidBuf, DM_UUID_LEN, err_func);
 
 /// Used as a parameter for functions that take either a Device name
