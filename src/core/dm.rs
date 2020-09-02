e467a42132d9e282c324140e56bbf492af16c49b
Bryan Gurney <bgurney@redhat.com>
2020-09-02 13:11:06 -0400

    parse_table_status: add same_item_push clippy allow
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- src/core/dm.rs
+++ src/core/dm.rs
@@ -553,6 +553,9 @@ impl DM {
     // will either be backwards-compatible, or will increment
     // DM_VERSION_MAJOR.  Since calls made with a non-matching major version
     // will fail, this protects against callers parsing unknown formats.
+    // (The targets.push() call triggers an erroneous lint error, as
+    //  detailed in rust-clippy issue #5902.)
+    #[allow(clippy::same_item_push)]
     fn parse_table_status(count: u32, buf: &[u8]) -> Vec<(u64, u64, String, String)> {
         let mut targets = Vec::new();
         if !buf.is_empty() {
