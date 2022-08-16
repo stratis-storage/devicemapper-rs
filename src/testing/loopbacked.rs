fc5101638f49be1f71a6315d18bb92c4ab2edd9b
mulhern <amulhern@redhat.com>
2022-08-16 10:20:00 -0400

    Update to edition 2021
    Remove unnecessary imports now included in prelude
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/testing/loopbacked.rs
+++ src/testing/loopbacked.rs
@@ -3,7 +3,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use std::{
-    convert::TryInto,
     fs::OpenOptions,
     io::{self, Seek, SeekFrom, Write},
     os::unix::io::AsRawFd,
