9e8050c768f57152ab27d419e48383e5f71f2bee
mulhern <amulhern@redhat.com>
2022-09-20 19:20:33 -0400

    Allow unused unsafe
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/core/device.rs
+++ src/core/device.rs
@@ -72,10 +72,12 @@ impl From<dev_t> for Device {
 impl From<Device> for dev_t {
     fn from(dev: Device) -> dev_t {
         #[cfg(target_os = "android")]
+        #[allow(unused_unsafe)] // No longer unsafe in libc 0.2.133.
         unsafe {
             makedev(dev.major as i32, dev.minor as i32)
         }
         #[cfg(not(target_os = "android"))]
+        #[allow(unused_unsafe)] // No longer unsafe in libc 0.2.133.
         unsafe {
             makedev(dev.major, dev.minor)
         }
